b71d9075c1729c796be81eb9ed4e88ea10073a72
yutanakano <yutanakano395@gmail.com>
2021-10-03 22:05:47 +0900

    Linterを設定 (#3)
    * :wrench: eslintの設定を追加
    
    * :heavy_plus_sign: prettierを追加
    
    npm install --save-dev prettier eslint-config-prettier
    
    * :wrench: vscodeで保存する時にesbenp.prettier-vscodeでlintされるようにした
    
    * :art: npm run format
    
    npm run format

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,22 +1,22 @@
-import { GetStaticProps, GetStaticPaths } from 'next'
+import { GetStaticProps, GetStaticPaths } from 'next';
 import { client } from '../../libs/client';
 import styles from '../../styles/Home.module.css';
 
 interface Article {
-  id: string
-  title: string
-  body: string
-  publishedAt: string
+  id: string;
+  title: string;
+  body: string;
+  publishedAt: string;
 }
 
 interface Contents {
-  contents: Article[]
+  contents: Article[];
 }
 
 export default function BlogId({
   blog,
 }: {
-  blog: { id: string; title: string; publishedAt: string; body: string }
+  blog: { id: string; title: string; publishedAt: string; body: string };
 }) {
   return (
     <main className={styles.main}>
@@ -34,16 +34,17 @@ export default function BlogId({
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths: GetStaticPaths = async () => {
-  const data: Contents = await client.get({ endpoint: "blog" });
+  const data: Contents = await client.get({ endpoint: 'blog' });
 
   const paths = data.contents.map((content) => `/blog/${content.id}`);
+
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context.params?.id as string;
-  const data = await client.get({ endpoint: "blog", contentId: id });
+  const data = await client.get({ endpoint: 'blog', contentId: id });
 
   return {
     props: {
