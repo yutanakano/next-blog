e2bbb14a87fd6e7f161ea2125c8878c2e908fd31
yutanakano <yutanakano395@icloud.com>
2021-10-10 00:55:06 +0900

    :sparkles: 画面を構成するコンポーネントを作成

---
+++ components/atoms/Article.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '../../styles/Home.module.css';
+
+export interface Props {
+  blog: { id: string; title: string; body: string; publishedAt: string };
+}
+
+export const Article = (props: Props) => {
+  return (
+    <article className='markdown-body'>
+      <h1 className={styles.title}>{props.blog.title}</h1>
+      <p className={styles.publishedAt}>{props.blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${props.blog.body}`,
+        }}
+        className={styles.post}
+      />
+    </article>
+  );
+};
