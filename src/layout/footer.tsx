769f34bc82c9ed5ba14194412b1be1fe6a1f4fc0
yutanakano <yutanakano395@icloud.com>
2021-11-02 02:24:20 +0900

    :fire: 現時点で利用しないnavを削除

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -5,34 +5,6 @@ export const Footer = () => {
   return (
     <footer className='max-w-screen-2xl px-4 md:px-8 mx-auto'>
       <div className='flex flex-col items-center border-t pt-6'>
-        <nav className='flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2 md:gap-6 mb-4'>
-          <Link href='#'>
-            <a className='text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100'>
-              About
-            </a>
-          </Link>
-          <Link href='#'>
-            <a className='text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100'>
-              Investor Relations
-            </a>
-          </Link>
-          <Link href='#'>
-            <a className='text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100'>
-              Jobs
-            </a>
-          </Link>
-          <Link href='#'>
-            <a className='text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100'>
-              Press
-            </a>
-          </Link>
-          <Link href='#'>
-            <a className='text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100'>
-              Blog
-            </a>
-          </Link>
-        </nav>
-
         <div className='flex gap-4'>
           <Link href='#'>
             <a
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -24,45 +24,6 @@ export const Header = () => {
               Flowrift
             </a>
           </Link>
-
-          <nav className='hidden lg:flex gap-12'>
-            <a
-              href='#'
-              className='text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100'
-            >
-              Home
-            </a>
-            <a
-              href='#'
-              className='inline-flex items-center text-indigo-500 text-lg font-semibold gap-1'
-            >
-              Features
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5 text-gray-800'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </a>
-            <a
-              href='#'
-              className='text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100'
-            >
-              Pricing
-            </a>
-            <a
-              href='#'
-              className='text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100'
-            >
-              About
-            </a>
-          </nav>
         </header>
       </div>
     </div>
