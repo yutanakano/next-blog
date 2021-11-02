f30c9eab0d6c701ca073d5453662bbd17339e38f
yutanakano <yutanakano395@icloud.com>
2021-11-03 03:28:23 +0900

    :lipstick: Headerのロゴを変更

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const Header = () => {
   return (
@@ -11,17 +12,13 @@ export const Header = () => {
               className='inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5'
               aria-label='logo'
             >
-              <svg
-                width='95'
-                height='94'
-                viewBox='0 0 95 94'
-                className='w-6 h-auto text-indigo-500'
-                fill='currentColor'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M96 0V47L48 94H0V47L48 0H96Z' />
-              </svg>
-              Flowrift
+              <Image
+                src='/icon.png'
+                alt='何が起こるかわからない'
+                width={95}
+                height={94}
+              />
+              何が起こるかわからない
             </a>
           </Link>
         </header>
