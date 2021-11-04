fb5ea24669ec9822b24017c3f61c25d290ce2c9b
yutanakano <yutanakano395@gmail.com>
2021-11-05 00:20:07 +0900

    Analyticsの導入 (#8)
    * :heavy_plus_sign: @types/gtag.js を追加
    
    npm install -D @types/gtag.js
    
    * :sparkles: ga対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../../libs/gtag';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Google Analyticsをページ遷移時にも対応させる
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,31 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../../libs/gtag';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head>
+          {/* Google Analytics */}
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async={true}
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });`,
+                }}
+              />
+            </>
+          )}
           <link
             rel='apple-touch-icon'
             sizes='180x180'
